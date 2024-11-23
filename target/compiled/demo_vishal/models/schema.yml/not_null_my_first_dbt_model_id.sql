f7855bcbe9a84785598abd50bfda673120473bca
Vishal Arora <vishal.arora@vishal.arora-GY7NV32WG4>
2024-11-23 15:31:36 +0530

    first commit

---
+++ target/compiled/demo_vishal/models/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `dbt-technical-training`.`alpha_test`.`my_first_dbt_model`
+where id is null
+
+
---
+++ target/compiled/demo_vishal/models/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `dbt-technical-training`.`alpha_test`.`my_second_dbt_model`
+where id is null
+
+
